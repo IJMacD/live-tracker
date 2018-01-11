9013909571dff88f279dd03adf6bc7078acdbde7
Iain MacDonald <IJMacD@gmail.com>
2018-01-11 10:00:32 +0800

    Don't hardcode share count

--- src/App.js
+++ src/App.js
@@ -11,6 +11,19 @@ const stock = "TSLA";
 const store = AlphaVantage(AV_API_KEY, stock);
 
 class App extends Component {
+	constructor (props) {
+		super(props);
+
+		this.state = {
+			shareCount: 0,
+		};
+
+		this.handleShareChange = this.handleShareChange.bind(this);
+	}
+
+	handleShareChange (e) {
+		this.setState({ shareCount: e.target.value });
+	}
 
 	componentDidMount () {
 		store.subscribe(() => this.forceUpdate());
@@ -22,7 +35,20 @@ class App extends Component {
 		return (
 			<div className="App">
 				<header className="App-header">
-					<h1 className="App-title">{stock}</h1>
+					<h1 className="App-title">
+						{stock}
+						<input
+							type="number"
+							value={this.state.shareCount}
+							min="0"
+							onChange={this.handleShareChange}
+							style={{
+								marginLeft: 10,
+								width: 100,
+								fontSize: 18,
+							}}
+						/>
+					</h1>
 				</header>
 				<div className="App-container">
 					<Graph values={store.state.values} />
@@ -30,11 +56,11 @@ class App extends Component {
 						{ store.state.value &&
 							<p>{store.state.value.toFixed(3)}</p>
 						}
-						{ store.state.value &&
-							<SmearValue value={store.state.value*49} format={usdFormat} />
+						{ store.state.value && this.state.shareCount > 0 &&
+							<SmearValue value={store.state.value*this.state.shareCount} format={usdFormat} />
 						}
-						{ store.state.value && store.state.exchangeRate &&
-							<SmearValue value={store.state.value*49*store.state.exchangeRate} format={gbpFormat} />
+						{ store.state.value && store.state.exchangeRate && this.state.shareCount > 0 &&
+							<SmearValue value={store.state.value*this.state.shareCount*store.state.exchangeRate} format={gbpFormat} />
 						}
 						{ store.state.updated &&
 							<p>{store.state.updated.toString().substr(0, 24)}</p>
